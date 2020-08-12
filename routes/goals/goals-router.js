00689e7cbc431c6c5f5940c968f0abefaecdf1d9
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-08-12 13:26:05 -0500

    finished goals router and endpoints

--- routes/goals/goals-router.js
+++ routes/goals/goals-router.js
@@ -1,29 +1,35 @@
 const router = require("express").Router();
-const knex = require("../config/knex-config")
+const knex = require("../config/knex-config");
 /**
  * User id is a param required
  * Gets a usrs goal weights and gets the goals.
  */
-router.get("/:id", (req,res)=> {
-knex.select("*")
-.where({userId:req.params.userId})
-.from("goals")
-.then(data =>{
-    console.log("data",data)
-    res.status(200).json(data)
-}).catch(err => {
-        res.status(500).json(err)
-})
-})
-router.post("/",(req,res) => {
-    knex("goals")
+router.get("/:id", (req, res) => {
+  knex
+    .select("*")
+    .from("goals")
+    .where({ userId: req.params.id })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+router.post("/", (req, res) => {
+  knex("goals")
     .insert({
-        userId:req.body.userId,
-        goalWeight:req.body.goalWeight,
-        goalDate:req.body.goalDate
-    }).returning("*")
-})
-
-
+      userId: req.body.userId,
+      goalWeight: req.body.goalWeight,
+      goalDate: req.body.goalDate,
+    })
+    .returning("*")
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
